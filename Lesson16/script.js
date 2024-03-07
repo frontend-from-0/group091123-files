a0b511946d2e0b7234c4edfd6085c23a7429f909
Anna S. A <anna.atalay@minna.tech>
2024-03-07 20:23:28 +0100

    Add lesson16 completed files

--- Lesson16/script.js
+++ Lesson16/script.js
@@ -1,9 +1,16 @@
 /*  
+Global scope
+
 Functions
 
 Functions are one of the fundamental building blocks of the language. They are reusable blocks of code that can be executed when needed. Functions encapsulate a set of statements and can be called or invoked to perform a specific task or calculation. 
 
 Function Declaration
+Anonymus function
+function () {
+  console.log('Hello world');
+}
+Anonymus function with parameters
 function (parameter1, parameter2) {
   return parameter1 * parameter2;
 };
@@ -70,18 +77,55 @@ switch (day) {
 }
 */
 
+let age = 51;
+if (age > 60) {
+	console.log('Senior citizen. Can vote!');
+} else if (age > 18) {
+	console.log('Can vote!');
+} else {
+	console.log('Can not vote!');
+}
+
 // 1. Write a function that takes in a number and checks if it's even or odd using if else statement.
 
+function isEvenOrOdd(number) {
+	const reminder = number % 2;
+	// === - strict equality
+	if (reminder === 0) {
+		return 'Even';
+	} else {
+		return 'Odd';
+	}
+}
 
+console.log('Exercise 1', isEvenOrOdd(9), isEvenOrOdd(2));
 // 2. Write a function that takes age as input. If the age is less than 18, print "Sorry, you are not old enough to vote". If the age is between 18 and 65 (inclusive), print "You are eligible to vote". If the age is greater than 65, print "You are a senior citizen".
+function canVote(age) {
+	if (age < 18) {
+		return 'Sorry, you are not old enough to vote';
+	} else if (18 <= age && age <= 65) {
+		return 'You are eligible to vote';
+	} else {
+		return 'You are a senior citizen';
+	}
+}
 
+console.log('Exercise 2', canVote(10), canVote(30), canVote(75));
 
 // 3. Write a function that asks the user to enter two numbers and checks if the first number is greater than, less than, or equal to the second number using if else statement.
 
 
 // 4. Write a function that takes in a string and checks if the first letter is uppercase using if else statement.
+function isFirstLetterUppercase(stringToCheck){
+const firstLetter = stringToCheck[0];
+  if (firstLetter === firstLetter.toUpperCase()) {
+    return true;
+  } else {
+    return false;
+  }
 
-
+}
+console.log(isFirstLetterUppercase("Televiision"));
 // 5. Write a function that takes in a number and checks if it's positive, negative, or zero using if else statement.
 
 // 6. Write a function that takes in a year and checks if it's a leap year (is divisible by 4 and not divisible by 100 OR divisible by 400) or not using if else statement.
@@ -105,6 +149,34 @@ switch (day) {
 // 12. Write a function that asks the user to enter their score on a test. If the score is less than 60, print "You failed the test". If the score is between 60 and 70 (inclusive), print "You passed the test, but your grade is not great". If the score is between 71 and 80 (inclusive), print "You got a B". If the score is between 81 and 90 (inclusive), print "You got an A". If the score is greater than 90, print "You got an A+ - Great job!".
 
 // 13. Write a function that asks the user to enter a month number (1-12) and displays the corresponding season name. Use a switch statement to handle the different cases.
+
+function getSeasonName(numberOfAMonth) {
+	switch (numberOfAMonth) {
+      // numberOfAMonth === 12
+		case 12:
+		case 1:
+		case 2:
+      // console.log('Winter');
+			// break;
+			return 'Winter';
+		case 3:
+		case 4:
+		case 5:
+			return 'Spring';
+		case 6:
+		case 7:
+		case 8:
+			return 'Summer';
+		case 9:
+		case 10:
+		case 11:
+			return 'Autumn';
+    default: 
+      return 'Value is not supported, please enter value between 1 and 12.';
+	}
+}
+
+console.log('Exercise 13', getSeasonName(1), getSeasonName(5), getSeasonName(20));
 
 // 14. Write a function that asks the user to enter a grade letter (A, B, C, D, or F) and displays a message indicating whether the grade is passing (A, B, C) or failing (D, F). Use a switch statement to handle the different cases.
 
