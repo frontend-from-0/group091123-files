31d1ee062e54565de5e16bd25d4370eece2b339f
Anna S. A <anna.atalay@minna.tech>
2024-03-28 21:09:15 +0100

    Add Lesson 29 placeholder files

---
+++ Lesson29/script.js
@@ -0,0 +1,33 @@
+
+// 13. Array Destructuring
+// Given the following array, use array destructuring to assign the values of x, y, and z to their respective variables: 
+const coordinates = [10, 20, 30];
+
+// 14. Object Destructuring
+// Given the following object, use object destructuring to assign the values of name and age to their respective variables:
+const personE14 = {
+  name: 'John Doe',
+  age: 25,
+};
+
+// 15. Array Destructuring with Default Values
+// Given the following array, use array destructuring with default values to assign the values of a, b, and c, with default values of 1, 2, and 3 respectively:
+const numbers = [4];
+
+
+// 16. Object Destructuring with Renaming
+// Given the following object, use object destructuring with renaming to assign the value of name to a variable named fullName:
+const personE16 = {
+  name: 'John Doe',
+};
+
+
+// 17. Nested Object Destructuring
+// Given the following nested object, use object destructuring to assign the values of name, age, and city to their respective variables:
+const personE17 = {
+  name: 'John Doe',
+  age: 25,
+  address: {
+    city: 'New York',
+  },
+};
