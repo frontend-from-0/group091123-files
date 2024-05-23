9c0ea4bcd1fe46193af9db3f335a4551d2422b13
Anna S. A <anna.atalay@minna.tech>
2024-05-23 21:04:40 +0200

    Add Lesson24 completed files

--- Lesson24/app.js
+++ Lesson24/app.js
@@ -1,2 +1,40 @@
 const phoneRegex = /^\+?9?0?\d{10}$/;
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', function (event) {
+	event.preventDefault();
+	let isFormCorrect = true;
+
+	const nameInput = document.getElementById('name');
+	const nameInputError = document.getElementById('nameError');
+
+	if (nameInput.value.length < 3) {
+		nameInputError.textContent = 'Name should be at least 3 characters long.';
+		isFormCorrect = false;
+	} else {
+		nameInputError.textContent = '';
+	}
+
+	const emailInputError = document.getElementById('emailError');
+	const emailInput = document.getElementById('email');
+  
+	if (!emailRegex.test(emailInput.value)) {
+		emailInputError.textContent = 'Please enter correct email address.';
+		isFormCorrect = false;
+	} else {
+		emailInputError.textContent = '';
+	}
+
+	const formData = {
+		name: nameInput.value,
+		email: emailInput.value,
+	};
+
+	if (isFormCorrect) {
+		console.table(formData);
+	} else {
+		console.error('Form incorrect.');
+	}
+});
