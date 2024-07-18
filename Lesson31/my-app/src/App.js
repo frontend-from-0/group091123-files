7f97f5f9f1d17f454b5b8f65ef267a6a838777a2
Anna S. A <anna.atalay@minna.tech>
2024-07-18 22:45:59 +0200

    Add Lesson31 completed files

---
+++ Lesson31/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// Named export
+export const App = () => {
+  // JSX code
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world!</h1>
+      </header>
+    </div>
+  );
+};
+
+// export default will export entire file. 
+// export default App;
---
+++ Lesson31/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// Default import
+// import App from './App';
+// Named import
+import {App} from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
