b524a4a76dfefd991f7ecd82aec6e39f5581d0c0
Anna S. A <anna.atalay@minna.tech>
2024-09-26 19:49:44 +0300

    Add Lesson40 starter files

---
+++ Lesson40/recipe-app/src/AppRouter.js
@@ -0,0 +1,67 @@
+import { Routes, Route } from 'react-router-dom';
+import { RecipeList } from './modules/recipes/RecipeList';
+import { SearchBar } from './modules/recipes/SearchBar';
+import { FeaturedRecipes } from './modules/recipes/FeaturedRecipes';
+import { RecipeDetails } from './modules/recipes/RecipeDetails';
+import { ProtectedRoute } from './ProtectedRoute';
+import { Login } from './modules/user/Login';
+/*
+Lesson 38 TODOs:
+1. Add recipes pages routes:
+  + One the main app page, only display featured information
+  + Add a Search dedicated page
+  - Add Categories page
+    - Create Recipe List page
+  + Add Recipe details page
+  - Add Create new recipe page
+2.  Implement Protected Routes:
+  - Create new recipe page
+  + Settings page
+  + User page
+3. Implement 404 Page (catch-all route for undefined paths):
+
+*/
+
+export const AppRouter = () => {
+	return (
+		<Routes>
+			<Route path='/login' element={<Login/>} />
+			<Route
+				path='/user'
+				element={
+					<ProtectedRoute>
+						<h1>User page</h1>
+					</ProtectedRoute>
+				}
+			/>
+
+			<Route
+				path='/settings'
+				element={
+					<ProtectedRoute>
+						<h1>Settings page</h1>
+					</ProtectedRoute>
+				}
+			/>
+
+			<Route path='/recipes'>
+				<Route
+					path='search'
+					element={
+						<>
+							<SearchBar />
+							<RecipeList />
+						</>
+					}
+				/>
+				<Route path=':recipeId' element={<RecipeDetails />} />
+			</Route>
+
+			<Route
+				path='/'
+				element={<FeaturedRecipes />}
+			/>
+      <Route path="*" element={<h1>Page not found</h1>}/>
+		</Routes>
+	);
+};
---
+++ Lesson40/recipe-app/src/shared-components/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import {  Link } from 'react-router-dom';
+import './styles.css';
+
+export const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			
+			<ul className='navbar__list'>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/recipes/search">Search Recipes</Link></li>
+				<li>Create New Recipe</li>
+				<li>Account </li>
+				<li>Settings</li>
+				<li><Link to='/login'>Log in</Link></li>
+				<li>Log out</li>
+			</ul>
+		</nav>
+	);
+};
