750cc5574f069ecb6aaf2b07a53e66ee0a327b1c
Anna S. A <anna.atalay@minna.tech>
2024-10-10 18:30:28 +0300

    Add Lesson41 starter files

---
+++ Lesson41/my-app/src/theme.ts
@@ -0,0 +1,9 @@
+import { Theme, createTheme } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
+
+export const theme: Theme = createTheme({
+	palette: {
+		primary: { main: blueGrey[500] },
+	},
+	spacing: 4,
+});
